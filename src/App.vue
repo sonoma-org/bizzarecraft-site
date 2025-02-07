f132ff0ef2d061cf660fc0896f5d2ba89f4c75b7
saddydead1 <luckybeyhuk@gmail.com>
2025-02-07 20:29:28 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Приватный сервер BizarreCraft</title>
     <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-  <nav class="navbar">
-        <ul>
-            <li><RouterLink to="/">Главная</RouterLink></li>
-            <li><RouterLink to="/play">Сборка</RouterLink></li>
-            <li><a href="https://map.bizarrecraft.ru">Карта</a></li>
-            <li><a href="https://discord.gg/GqfzJPSQHk">Discord</a></li>
-            <li><a href="https://github.com/sonoma-org/bizzarecraft-site">GitHub</a></li>
-        </ul>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
-    <footer class="footer">
-      <p style="font-size: medium;">Not an official Minecraft product. We are in no way affiliated with or endorsed by Mojang Synergies AB, Microsoft Corporation or other rightsholders.</p>
-        <p>&copy; 2025 MIT License | <a href="https://sonoma.su">Sonoma Org.</a></p>
-    </footer>
-</body>
+  </head>
+  <body>
+    <nav class="navbar">
+          <ul>
+              <li><RouterLink to="/">Главная</RouterLink></li>
+              <li><RouterLink to="/play">Играть</RouterLink></li>
+              <li><a href="https://map.bizarrecraft.ru">Карта</a></li>
+              <li><a href="https://discord.gg/GqfzJPSQHk">Discord</a></li>
+              <li><a href="https://github.com/sonoma-org/bizzarecraft-site">GitHub</a></li>
+          </ul>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+      <footer class="footer">
+        <p style="font-size: small;">Not an official Minecraft product. We are in no way affiliated with or endorsed by Mojang Synergies AB, Microsoft Corporation or other rightsholders.</p>
+          <p style="font-size: small;">&copy; 2025 <a href="https://sonoma.su">Sonoma Org.</a></p>
+      </footer>
+  </body>
 </template>
--- src/components/Play.vue
+++ src/components/Play.vue
@@ -2,10 +2,14 @@
             <div class="container">
       <img src="/public/bizarrecraft2.png" width="500">
         <br><br>
+        <h2>Добро пожаловать на 2 сезон!</h2>
+        <br>
+        <p>Для того, чтобы играть на нашем сервере вам нужно написать нам в сообществе Discord! Мы обязательно рассмотрим вашу заявку.</p>
+        <p>Также вам необходимо установить данные моды:</p>
         <a href="https://nextcloud.sonoma.su/s/Mjq5LQGe2GtNFXG">
-        <button class="main-btn">Скачать моды</button></a> <br> <br>
+        <button class="main-btn">Скачать EmoteCraft</button></a> <br> <br>
         <a href="https://nextcloud.sonoma.su/s/TGezFJgyj4mz8xC">
-        <button class="main-btn">Скачать сборку для Prism</button></a>
-        <p style="font-size: smaller; padding-top: 10px;">Для игры нужен Fabric 1.21.4</p>
+        <button class="main-btn">Скачать Simple Voice Chat</button></a>
+        <p style="font-size: smaller; padding-top: 10px;">Для игры нужна версия 1.21.4</p>
     </div>
 </template>
