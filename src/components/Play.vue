ab48675da92aa7b362fb0b11e94004381ad01464
saddydead1 <luckybeyhuk@gmail.com>
2025-02-23 17:16:20 +0300

    done

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -12,23 +12,23 @@
         <div class="card">
           <h3>NeoForge</h3>
           <p>Новый форк Forge</p>
-          <p>Оптимизирована Embedium</p>
+          <p>Оптимизирована Sodium</p>
           <p>Все фишки OptiFine уже есть!</p>
-          <button>Скачать</button>
+          <button onclick="toastr.error('В разработке')">Скачать</button>
         </div>
         <div class="card highlight">
           <h3>Fabric</h3>
           <p>Предпочтительный вариант</p>
           <p>Оптимизирована Sodium</p>
           <p>Стоят абсолютно все нужные моды!</p>
-          <button>Скачать</button>
+          <button onclick="location.href='https://nextcloud.sonoma.su/s/byzWrRgCZoSnZ68/download/BizarreCraft%20%28Fabric%29.zip'">Скачать</button>
         </div>
         <div class="card">
           <h3>Quilt</h3>
           <p>Легкий форк Fabric</p>
           <p>Оптимизирована Sodium</p>
           <p>Нужные тебе моды уже есть!</p>
-          <button>Скачать</button>
+          <button onclick="toastr.error('В разработке')">Скачать</button>
         </div>
       </div>
       <br>
@@ -36,7 +36,6 @@
         <summary>Или скачай нужные моды самостоятельно тут</summary>
         <a href="https://modrinth.com/plugin/simple-voice-chat">Simple Voice Chat</a> <br>
         <a href="https://modrinth.com/plugin/emotecraft">EmoteCraft</a> <br>
-        <a href="https://modrinth.com/plugin/emotecraft">EmoteCraft Tweaks</a> <br>
         <a href="https://modrinth.com/mod/voicechat-soundboard">Voice Chat Soundboard</a> <br>
         <a href="https://modrinth.com/mod/cpmsvcc">CPM Simple Voice Chat compat</a> <br>
         <a href="https://modrinth.com/plugin/custom-player-models">Customizable Player Models</a> <br>
